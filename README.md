69b6d7b4e33e5f1c6c0784a9cd854e397a050178
Silvio Ramalho <silvioramalho@gmail.com>
2019-12-11 11:16:47 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Python Async Solution with asyncio, aiohttp and redis
+
+This is a simple sample code created to show how I can use Asynchronous Python lib, ASYNCIO, consuming an API with AIOHTTP and writing the results in REDIS.
+
+`Note: I am not handling exceptions yet.`
+
+# Async Solution Sample
+
+## Install Solution
+
+### Clone Solution
+
+> git clone git@github.com:silvioramalho/python-async-solution.git
+
+> cd python-async-solution
+
+> python3 -m venv env
+
+> . env/bin/activate
+
+> pip install --upgrade pip
+
+> pip install -r requirements.txt
+
+
+## Create Docker with REDIS
+
+> docker-compose up -d
+
+`Note: You can change the password within docker-compose.yml before running this command. Remember to change it also within the code(Line 10).`
+
+### Current Connection Info
+
+```
+host: localhost
+port: 6379
+password = kjhKJHg4!
+```
+
+## Run Application 
+
+> python src/app.py
+
+
+
+
---
+++ src/app.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+import time
+import random
+import asyncio
+import aiohttp
+import redis
+
+URL = 'https://api.github.com/events'
+MAX_THREADS = 100
+redis = redis.Redis('localhost', port=6379, db=0, password='kjhKJHg4!')
+
+async def fetch_async(pid, session):
+    start = datetime.now()
+    sleepy_time = random.randint(2, 5)
+    print('Async process start at {}, waiting for {} seconds'.format(
+        start.strftime("%Y-%m-%d, %H:%M:%S"), sleepy_time))
+    async with session.get(URL) as response:
+        return await response.json()
+
+@asyncio.coroutine
+async def asynchronous():
+    start = time.time()
+    async with aiohttp.ClientSession() as session:
+        futures = [fetch_async(i, session) for i in range(1, MAX_THREADS + 1)]
+        for i, future in enumerate(asyncio.as_completed(futures)):
+            result = await future
+            # Here we can make calculations
+            if (type(result) is list):
+                pos = random.randint(1, 10)
+                data = result[pos].get('id')
+            else:
+                data = result.get('message')
+
+            print('{} {}'.format(">>" * (i + 1), data))
+            
+            # Insert in REDIS LIST
+            redis.rpush('dates', data)
+
+    print("The process took: {:.2f} seconds".format(time.time() - start))
+
+ioloop = asyncio.get_event_loop()
+ioloop.run_until_complete(asynchronous())
+ioloop.close()
